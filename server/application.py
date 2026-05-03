6d18b47d853801fe2f0d6cf9ad0637328611a558
IamPMelnikov <114242431+IamPMelnikov@users.noreply.github.com>
2026-05-03 18:02:33 +0500

    update

--- server/application.py
+++ server/application.py
@@ -4,10 +4,10 @@ import socketserver
 PORT=8000
 
 class TestMe():
-    def take_five():
-	return 5
+    def take_five(self): # Добавлен self
+        return 5
     def port(self):
-	return PORT
+        return PORT
 
 if __name__=='__main__':
     Handler = http.server.SimpleHTTPRequestHandler
